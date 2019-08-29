845b7acc55084a157a67b6c5d0d0586b15d36e65
Gavrilin <telekolki@gmail.com>
2019-08-29 15:59:48 +0300

    move parsing to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './items-list/item.model';
+declare var require: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'items-info-portal';
+
+  jsonDataString = JSON.stringify(require('src/app/items-list/items-data.json'));
+  jsonDataParsed: Item[] = JSON.parse(this.jsonDataString);
+  itemsArray: Item[] = [];
+
+
+  convertJsonToArray() {
+    for (const item of this.jsonDataParsed) {
+      this.itemsArray.push(new Item(item.name, item.sku, item.quantity, item.orders));
+    }
+  }
+
+  ngOnInit() {
+
+    console.log(this.jsonDataString);
+    console.log(this.jsonDataParsed);
+    this.convertJsonToArray();
+    console.log(this.itemsArray);
+  }
 }
--- src/app/items-list/items-list.component.ts
+++ src/app/items-list/items-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Item } from './item.model';
-declare var require: any
+
 
 @Component({
   selector: 'app-items-list',
@@ -8,26 +7,7 @@ declare var require: any
   styleUrls: ['./items-list.component.css']
 })
 export class ItemsListComponent implements OnInit {
-
-  jsonDataString = JSON.stringify(require('src/app/items-list/items-data.json'));
-  jsonDataParsed: Item[] = JSON.parse(this.jsonDataString);
-  itemsArray: Item[] = [];
-
-
-  convertJsonToArray() {
-    for (const item of this.jsonDataParsed) {
-      this.itemsArray.push(new Item(item.name, item.sku, item.quantity, item.orders));
-    }
-  }
-
   constructor() { }
 
-  ngOnInit() {
-
-    console.log(this.jsonDataString);
-    console.log(this.jsonDataParsed);
-    this.convertJsonToArray();
-    console.log(this.itemsArray);
-  }
-
+  ngOnInit() { }
 }
